b5206bc788ebaeb123ea19d0b96ca642ea31713f
Artur dos Santos Shon <sky_artur@hotmail.com>
2024-10-19 10:45:37 -0300

    Definição do banco de dados da aplicação.

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,15 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 from ._factory import Database
+
+
+load_dotenv(dotenv_path=Path(__file__).parent.parent / '.env')
+
+
+dbDev = Database(
+    'mysql',
+    database=os.getenv("DATABASE"),
+    user=os.getenv("USER"),
+    password=os.getenv("PASSWORD"),
+)
--- database/_objects.py
+++ database/_objects.py
@@ -1,17 +1,9 @@
-import os
-from dotenv import load_dotenv
-from pathlib import Path
 from ._singleton import *
-
-
-load_dotenv(dotenv_path=Path(__file__).parent.parent / '.env')
 
 
 class SQLiteConnector(Connector):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        if not kwargs:
-            self.params = {'database': f'{os.getenv("DATABASE")}.sqlite3'}
 
     def connect(self) -> sqlite3.connect:
         return sqlite3.connect(**self.params)
@@ -20,12 +12,6 @@ class SQLiteConnector(Connector):
 class MySQLConnector(Connector):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        if not kwargs:
-            self.params = {
-                'database': os.getenv('DATABASE'),
-                'user': os.getenv('USER'),
-                'password': os.getenv('PASSWORD'),
-            }
 
     def connect(self) -> mysql.connector.connect:
         return mysql.connector.connect(**self.params)
@@ -34,12 +20,6 @@ class MySQLConnector(Connector):
 class PostgreSQLConnector(Connector):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        if not kwargs:
-            self.params = {
-                'database': os.getenv('DATABASE'),
-                'user': os.getenv('USER'),
-                'password': os.getenv('PASSWORD'),
-            }
 
     def connect(self) -> psycopg2.connect:
         return psycopg2.connect(**self.params)
