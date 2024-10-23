87636cfa7f89282ffd921c0c92e6ae20947173ea
Artur dos Santos Shon <sky_artur@hotmail.com>
2024-10-23 19:11:57 -0300

    Adição de um campo para gênero.

--- views/_view_new_profile.py
+++ views/_view_new_profile.py
@@ -1,29 +1,33 @@
 import flet as ft
 from typing import Callable
-from components import app_window, app_page, top_heading, text_field
+from components import app_window, app_page, top_heading, text_field, dropdown_field
 from validators import *
 from functions import clear_fields, save_profile
 
 
-def view_new_profile(page: ft.Page, view_login: Callable) -> None:
+def view_new_profile(page: ft.Page, view_login: Callable = None) -> None:
 
     def save(event):
-        if not validator_fields(page, *_fields): return
+        if not validate_fields(page, *_fields): return
         elif not (_name := validate_name(page, _fields[0])): return
         elif not (_birth := validate_date(page, _fields[1])): return
         elif not (_username := validate_username(page, _fields[2])): return
         elif not (_email := validate_email(page, _fields[3])): return
         elif not (_password := validate_password(page, _fields[4], _fields[5])): return
-        elif not (_weight := validate_biometric(page, _fields[6], 'weight')): return
-        elif not (_height := validate_biometric(page, _fields[7], 'height')): return
+        elif not (_gender := validate_gender(page, _fields[6])): return
+        elif not (_weight := validate_biometric(page, _fields[7], 'weight')): return
+        elif not (_height := validate_biometric(page, _fields[8], 'height')): return
         else:
-            save_profile(page, _name, _birth, _username, _email, _password, _weight, _height)
+            save_profile(page, _name, _birth, _username, _email, _password, _gender, _weight, _height)
+            view_login(page, view_new_profile)
         clear_fields(page, *_fields)
 
-    app_window(page, 700, 490)
+    page.controls.clear()
+    app_window(page, 700, 560)
     app_page(page)
     top_heading(page, 'Register a new user')
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
     _fields = [
         text_field('Name', width=440, tooltip='Alphabetic characters only.'),
         text_field('Birth Date', width=200, hint_text='DD/MM/YYYY'),
@@ -31,6 +35,7 @@ def view_new_profile(page: ft.Page, view_login: Callable) -> None:
         text_field('Email', width=440, hint_text='example@email.com'),
         text_field('Password', width=318, password=True, tooltip='Minimum of 6 characters.'),
         text_field('Confirm Password', width=318, password=True),
+        dropdown_field('Gender', ['M', 'F']),
         text_field('Weight', width=160, suffix_text='kilograms', tooltip='Use a comma (,) to separate decimal places.'),
         text_field('Height', width=160, suffix_text='meters', tooltip='Use a comma (,) to separate decimal places.'),
     ]
